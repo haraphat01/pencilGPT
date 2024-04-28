24d110db99f50983132b821ff5bfc3abd3746e92
haraphat01 <haraphat@gmail.com>
2024-04-28 00:54:44 -0700

    updated the prompt

--- src/app/components/getGroqChatCompletion.js
+++ src/app/components/getGroqChatCompletion.js
@@ -1,6 +1,6 @@
 "use strict";
 const Groq = require("groq-sdk");
-let prompt = "Review the links and the news sources provided about the cryptocurrency token mentioned and provide a detailed fundamental and sentimental analysis based on the context provided. Your analysis should cover various aspects such as the tokenomics, project team, community engagement, development roadmap, market trends, and potential risks. Ensure to provide references from credible sources cited in the document to support your analysis. Pay attention to the token's historical performance, current market sentiment, technological advancements, partnerships, regulatory compliance, and any other relevant factors impacting its value and adoption.";
+let prompt = "Review the links and the news sources provided about the cryptocurrency token mentioned and provide a detailed fundamental, price movements, using technical indicators and sentimental analysis based on the context provided. Your analysis should cover various aspects such as the tokenomics, project team, community engagement, development roadmap, market trends, technical analysis, and potential risks. Ensure to provide references from credible sources cited in the document to support your analysis. Pay attention to the token's historical performance, current market sentiment, technological advancements, partnerships, regulatory compliance, and any other relevant factors impacting its value and adoption.";
 async function getGroqChatCompletion(content) {
     const groq = new Groq({
         apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY
